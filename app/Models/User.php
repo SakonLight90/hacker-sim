f5730977461c7b756b48ff8c93727c713e563847
SakonLight90 <66551324+SakonLight90@users.noreply.github.com>
2025-06-27 15:39:05 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,5 +1,42 @@
-// ... altre importazioni
-public function software()
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class User extends Authenticatable
 {
-    return $this->hasMany(\App\Models\Software::class);
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+        'ip_address',
+        'phase',
+        'money_clean',
+        'money_dirty',
+        'crypto_legal',
+        'crypto_illegal',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Relation: User has many Software
+     */
+    public function software()
+    {
+        return $this->hasMany(Software::class);
+    }
 }
