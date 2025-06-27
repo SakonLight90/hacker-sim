3e0b601c869ffeb110d2a38fb2c65b1f3c0f6028
SakonLight90 <66551324+SakonLight90@users.noreply.github.com>
2025-06-27 15:40:23 +0200

    Update index.blade.php

--- resources/views/software/index.blade.php
+++ resources/views/software/index.blade.php
@@ -2,10 +2,17 @@
 
 @section('content')
     <h2>Software installati</h2>
+    @if(session('success'))
+        <div class="status-bar">{{ session('success') }}</div>
+    @endif
+    @if(session('error'))
+        <div class="status-bar" style="color:#ff3333">{{ session('error') }}</div>
+    @endif
     <table>
         <tr>
             <th>Software</th>
             <th>Livello</th>
+            <th>Punteggio Bonus</th>
             <th>Azione</th>
         </tr>
         @foreach($software as $sw)
@@ -13,9 +20,23 @@
                 <td>{{ ucfirst($sw->type) }}</td>
                 <td>{{ $sw->level }}</td>
                 <td>
+                    @php
+                        $bonus = [
+                            'sdk' => 5,
+                            'spam' => 5,
+                            'adware' => 4,
+                            'firewall' => 3,
+                            'antivirus' => 3,
+                            'ip_spoofer' => 3,
+                            'scanner' => 3,
+                        ];
+                    @endphp
+                    +{{ $bonus[$sw->type] * $sw->level }}
+                </td>
+                <td>
                     <form method="POST" action="{{ route('software.upgrade', $sw->type) }}">
                         @csrf
-                        <button type="submit">Upgrade</button>
+                        <button type="submit">Upgrade ({{ $sw->level * 1000 }} $)</button>
                     </form>
                 </td>
             </tr>
