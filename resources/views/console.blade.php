4b7be36e09ab7c138fa1fe7f5809dc9262639d13
SakonLight90 <66551324+SakonLight90@users.noreply.github.com>
2025-06-27 15:41:37 +0200

    Create console.blade.php

---
+++ resources/views/console.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.terminal')
+
+@section('content')
+    <h2>Benvenuto nella tua console Hacker Sim!</h2>
+    <p>Usa il menu laterale per navigare tra le funzioni principali.</p>
+@endsection
